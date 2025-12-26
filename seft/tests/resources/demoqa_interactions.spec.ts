7dec4609ff64bfdb141c5bfff4fd240dbd0836d4
Nhung Huynh <nhunghuynh@kms-technology.com>
2025-12-27 00:56:57 +0700

    add exercise Resizable and Droppable

--- seft/tests/resources/demoqa_interactions.spec.ts
+++ seft/tests/resources/demoqa_interactions.spec.ts
@@ -50,7 +50,7 @@ test.describe('Practice elemements on demoqa page - Interactions', () => {
         await page.getByRole('listitem').filter({hasText: 'Selectable'}).click();
 
         //Select Item 1 and Item 3 from the list
-        const listItems = page.locator('#demo-tabpane-list .list-group-item');
+        const listItems = page.locator('#verticalListContainer').getByRole('listitem');
 
         const item1 = listItems.filter({hasText: 'Cras justo odio'});
         const item3 = listItems.filter({hasText: 'Morbi leo risus'});
@@ -68,8 +68,8 @@ test.describe('Practice elemements on demoqa page - Interactions', () => {
         const gridItems = page.locator('#demo-tabpane-grid .list-group-item');
 
         //Select Item 2 and Item 5 from the grid
-        const gridItem2 = gridItems.filter({hasText: 'Dapibus ac facilisis in'});
-        const gridItem5 = gridItems.filter({hasText: 'Vestibulum at eros'});
+        const gridItem2 = gridItems.filter({hasText: 'Two'});
+        const gridItem5 = gridItems.filter({hasText: 'Five'});
 
         await gridItem2.click();
         await gridItem5.click();
@@ -77,5 +77,47 @@ test.describe('Practice elemements on demoqa page - Interactions', () => {
         //Verify that Item 2 and Item 5 are selected
         await expect(gridItem2).toHaveClass(/active/);
         await expect(gridItem5).toHaveClass(/active/);
+    });
+
+    //Test03: Resizable
+    test('Test03: Resizable', async ({page}) => {
+        //Click Resizable button on the left menu
+        await page.getByRole('listitem').filter({hasText: 'Resizable'}).click();
+
+        //Resize the box by dragging the handle
+        const resizeBox = page.locator('#resizableBoxWithRestriction');
+        const handle = resizeBox.locator('.react-resizable-handle');
+
+        const width = await resizeBox.evaluate(el => el.clientWidth);
+        const height = await resizeBox.evaluate(el => el.clientHeight);
+        console.log(width, height);
+
+        await handle.click();
+        await page.mouse.down()
+        await page.mouse.move(width-100, height-100); // Move to new position
+        await page.mouse.up();
+
+        //Verify the new size of the box is smaller than the original size
+        const width1 = await resizeBox.evaluate(el => el.clientWidth);
+        const height1 = await resizeBox.evaluate(el => el.clientHeight);
+
+        console.log(width1, height1);
+
+        expect(width1).toBeLessThan(width);
+        expect(height1).toBeLessThan(height);
+    });
+
+    //Test04: Droppable
+    test('Test04: Droppable', async ({page}) => {
+        //Click Droppable button on the left menu
+        await page.getByRole('listitem').filter({hasText: 'Droppable'}).click();
+        
+        //Drag the draggable box and drop it into the droppable box
+        const draggable = page.locator('#draggable');
+        const droppable = page.locator('#droppable');
+        await draggable.dragTo(droppable);
+        
+        //Verify that the droppable box text has changed to "Dropped!"
+        await expect(droppable).toHaveText('Dropped!');
     });
 });
